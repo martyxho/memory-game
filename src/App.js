69753795f75851ac81fbbf66837e8c55c487b348
Marty <maruri2424@gmail.com>
2022-08-03 12:57:01 -0700

    added all flower pics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React from "react";
 import Card from "./components/Card";
 import './style/style.css';
 import nemophilia from './assets/nemophilia.jpg'
+import carnation from './assets/carnation.webp'
+import chrysanthemum from './assets/chrysanthemum.jpg'
+import lily from './assets/lily.jpeg'
+import orchid from './assets/orchid.jpg'
+import rose from './assets/rose.jpeg'
+import sakura from './assets/sakura.jpg'
+import sunflower from './assets/sunflower.jpeg'
+import tulip from './assets/tulip.jpg'
+import violet from './assets/violet.jpeg'
+
 
 function App() {
   return (
@@ -12,17 +22,15 @@ function App() {
       </div>
       <div id="main">
         <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
-        <Card src={nemophilia} name='nemophilia'/>
+        <Card src={carnation} name='carnation'/>
+        <Card src={chrysanthemum} name='chrysanthemum'/>
+        <Card src={lily} name='lily'/>
+        <Card src={orchid} name='orchid'/>
+        <Card src={rose} name='rose'/>
+        <Card src={sakura} name='sakura'/>
+        <Card src={sunflower} name='sunflower'/>
+        <Card src={tulip} name='tulip'/>
+        <Card src={violet} name='violet'/>
       </div>
     </div>
   );
